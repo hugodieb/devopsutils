471904b490fe7608ee87f376cee97e6aaf047a48
hugodieb <hugodieb.hd@gmail.com>
2020-09-21 12:15:59 -0300

    update / start / stop

---
+++ remotebin/stopstart.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+app=$1
+version=$2
+environ=$3
+service=$4
+DKPARAMS="${@:5}"
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source $SCRIPTPATH/common.sh
+source_env_app $app $environ
+
+dkstopstart(){
+    containername=${canonized_app}_${environ}_${service}
+    dkdata="$HOME/dockerdata/${app}_${environ}"
+    image=$app:$environ
+    envfile=~/${canonized_app}_${environ}.env
+    docker exec $containername stop_${service}.sh || true
+    docker stop $containername || true
+    docker rm $containername || true
+    echo "stopstart with $DKPARAMS"
+    docker run $DKPARAMS -d --name=$containername --env-file=$envfile -v $dkdata:/dkdata $image start_${service}.sh
+    exitcode=$?
+    return $exitcode
+}
+
+# globals
+set -e
+canonized_app=${app/\//_}
+dkstopstart
