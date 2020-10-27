a7886f956088e549ee5861c1b72a585f0a983e9e
hugodieb <hugodieb.hd@gmail.com>
2020-10-27 11:08:51 -0300

    comando para migrar as tabelas do banco

---
+++ bin/lmigratedb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+app=$1
+version=$2
+environ=$3
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source $SCRIPTPATH/common.sh
+
+source_app_env $app $environ
+
+set -e
+if [ "$MIGRATION_HOST" ]; then
+    echo "[MIGRATION] host pra rodar migration: $MIGRATION_HOST"
+    ssh -o StrictHostKeyChecking=no $MIGRATION_HOST dockerutils/remotebin/migratedb.sh $app $version $environ
+    echo "[MIGRATION] Executou migration no host: $MIGRATION_HOST"
+else
+    echo "[MIGRATION] Skipando migration pq a envvar MIGRATION_HOST nao esta definida"
+fi
---
+++ remotebin/migratedb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+app=$1
+version=$2
+environ=$3
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source $SCRIPTPATH/common.sh
+source_app_env $app $environ
+
+dkmigratedb(){
+    containername=${app}_${environ}_migratedb
+    image=$app:$environ
+    envfile=~/${app}_${environ}.env
+    docker run --rm --name=$containername --env-file=$envfile $image migratedb.sh
+    exitcode=$?
+    return $exitcode
+}
+
+set -e
+dkpull $app $version $environ
+dkmigratedb
